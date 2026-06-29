6f007e810ca4e86d438a84177dbe14e751691a46
Suribabuchitti <chittisuribabu02@gmail.com>
2026-06-29 12:38:30 +0530

    git hubpages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/User-Management-Dashboard/'
-})
+  base: "/User_Management_sys/",
+});
